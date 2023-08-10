24b1a5068c4d7c4745b21d5b5b719461f505c04d
gyeong <okbamtol@naver.com>
2023-08-10 20:38:15 +0900

    8.10 과제

--- SeSSacWeek4/TranslationViewController.swift
+++ SeSSacWeek4/TranslationViewController.swift
@@ -10,62 +10,81 @@ import Alamofire
 import SwiftyJSON
 
 class TranslationViewController: UIViewController {
-
+    
     @IBOutlet var translateTextView: UITextView!
     @IBOutlet var requestButton: UIButton!
     @IBOutlet var originalTextView: UITextView!
     
+    @IBOutlet var translateButton: UIButton!
+    var langCode = ""
+    let header: HTTPHeaders = [
+        "X-Naver-Client-Id": "\(APIKey.naverClientId)",
+        "X-Naver-Client-Secret": "\(APIKey.naverClientPw)"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         originalTextView.text = ""
         translateTextView.text = ""
         translateTextView.isEditable = false
         
     }
     
-
+    
     @IBAction func requestButtonClicked(_ sender: UIButton) {
         
-        let header: HTTPHeaders = [
-            "X-Naver-Client-Id": "\(APIKey.naverClientId)",
-            "X-Naver-Client-Secret": "\(APIKey.naverClientPw)"
+        //        let header: HTTPHeaders = [
+        //            "X-Naver-Client-Id": "\(APIKey.naverClientId)",
+        //            "X-Naver-Client-Secret": "\(APIKey.naverClientPw)"
+        //        ]
+        // var langCode = ""
+        let query = self.originalTextView.text ?? "nil값입니다."
+        let url1 = "https://openapi.naver.com/v1/papago/detectLangs"
+        let parameters1: Parameters = [
+            "query": query
         ]
-        var langCode = ""
-        let query = originalTextView.text ?? "nil값입니다."
-        print(query)
-        print(type(of: query))
-        let url1 = "https://openapi.naver.com/v1/papago/detectLangs?query=\(query)"
-       
-        AF.request(url1, method: .post, headers: header).validate().responseJSON { response in
+        
+        
+        AF.request(url1, method: .post, parameters: parameters1, headers: header).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
                 print("JSON: \(json)")
                 
-                //langCode = json["langCode"].stringValue
-                //print(langCode)
+                self.langCode = json["langCode"].stringValue
+                //print(langCode,"======")
+                self.translateTextView.text = "\(self.langCode)"
                 
-            case .failure(let error):
+            case.failure(let error):
                 print(error)
             }
         }
         
+        // print(langCode,"aaaaaaaaaaaa")
+        // print(type(of: langCode),"bbbbbbbbbbbb")
+        
+    }
+    
+    
+    
+    
+    @IBAction func translateButtonClicked(_ sender: UIButton) {
         
         let url = "https://openapi.naver.com/v1/papago/n2mt"
-        let parameters: Parameters = [
-            "source": "ko",     //"\(langCode)",
+        var parameters: Parameters = [
+            "source": "\(langCode)",
             "target": "en",
-            "text": originalTextView.text ?? "오류"
+            "text": self.originalTextView.text ?? "nil값입니다."
             
         ]
         
         AF.request(url, method: .post, parameters: parameters, headers: header).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
+                
                 let json = JSON(value)
                 print("=================","아래는 번역 API" )
-                print("JSON: \(json)")
+                // print("JSON: \(json)")
                 
                 let data = json["message"]["result"]["translatedText"].stringValue
                 self.translateTextView.text = data
@@ -75,12 +94,21 @@ class TranslationViewController: UIViewController {
             case .failure(let error):
                 print(error)
             }
+            
+            
         }
-
-        
         
         
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
